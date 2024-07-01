07ab621a7c2e60268d2d4fa5aa82311573c56598
Ahmed4Magdy <ahmedmagdi14402@gmail.com>
2024-07-02 00:05:25 +0300

    Hello my first repo

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+
+package com.mycompany.project1;
+
+
+import java.util.*;
+
+public class HelloWorld {
+    
+     public static void main(String [] args){
+
+//    Scanner input = new Scanner(System.in);
+
+     Display();
+    
+
+     }
+     
+     public static void Display(){
+         
+         
+         System.out.println("Hello World");
+         
+     }
+     
+
+}
+
